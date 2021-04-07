3dcb9b9a60eb82b644926a63c713de6326d6e63f
Carlos Selamtaw Gebru <selamtawcarlos@gmail.com>
2021-04-07 08:06:10 -0500

    w2d3 - javascript lab assignment - Inheritance

--- w2d3/main.js
+++ w2d3/main.js
@@ -23,6 +23,43 @@ let john = Object.create(person);
 john.setName("John");
 
 john.dateOfBirth = new Date(1998, 11,10);
-console.log(john.toString());
+
+const birthDateFormatted = `${john.dateOfBirth.getFullYear()}-${john.dateOfBirth.getMonth()+1 }-${john.dateOfBirth.getDate()}`;
+console.log(`The person's name is ${john.name}\n${john.name} was born on ${birthDateFormatted}`)
 
 // problem 2
+let employee = {
+    salary: 0,
+    hireDate: new Date(),
+    doJob: function (jobTitle) {
+        console.log(`${this.person.getName()} is a Programmer who earns $${this.salary} `);
+    }
+};
+
+let anna = Object.create(employee);
+anna.salary = 249995.50;
+anna.person = person;
+anna.person.setName("Anna");
+anna.doJob("Programmer");
+
+
+// problem 3
+function Person () {
+    this.name = "";
+    this.dateOfBirth = null;
+    this.getName = function () {
+        return this.name;
+    };
+    this.setName =function (newName) {
+        this.name = newName;
+    };
+    this.toString = function () {
+        const birthDateFormatted = `${this.dateOfBirth.getFullYear()}-${this.dateOfBirth.getMonth()+1 }-${this.dateOfBirth.getDate()}`;
+        return `Name: ${this.name} ${this.name} DateOfBirth: ${birthDateFormatted}`;
+    };
+}
+
+let peter = new Person();
+peter.setName("Peter");
+peter.dateOfBirth = new Date(1985, 11,10);
+console.log(peter.toString());
